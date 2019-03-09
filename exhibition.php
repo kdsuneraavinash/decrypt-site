3feff3caee0743cd3df3e4f1d74922df03cf1104
kdsuneraavinash <kdsuneraavinash@gmail.com>
2019-03-09 21:21:40 +0530

    Added exhibition

--- exhibition.php
+++ exhibition.php
@@ -26,108 +26,47 @@
                 <div class="row">
                     <div class="col-md-2"></div>
                     <div class="col-md-8">
-                        <h3><strong>Decrypt Exhibition?</strong></h3>
+                        <h3><strong>Decrypt Exhibition</strong></h3>
                         <p>
-                            Lorem ipsum
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consequat diam quis ligula
+                            sodales sagittis. Fusce vitae dui magna. Etiam in ex non velit fermentum dictum quis quis
+                            velit. Sed dictum, ligula at sollicitudin consectetur, nisi libero venenatis ligula, eget
+                            ullamcorper sapien urna eget diam. Sed blandit, arcu a mollis fringilla, libero justo
+                            scelerisque diam, quis laoreet ipsum diam eget purus. Donec pellentesque volutpat cursus.
+                            Donec consequat congue turpis, et eleifend enim sagittis eu. Nulla facilisi. Donec id
+                            pulvinar magna. Etiam in euismod ipsum, semper varius mauris. Integer nec ex ac erat mollis
+                            porttitor.
                         </p>
                         <br/>
                         <h3><strong>Who can participate?</strong></h3>
-                        <p>Ideathon is an opportunity exclusively available for students of the '18 Batch to showcase their
-                            skills. Groups consisting of 3-4 students of the '18 Batch of University of Moratuwa can
-                            participate in the competition.
+                        <p>
+                            Nulla vulputate purus nec felis lobortis iaculis. Maecenas egestas iaculis urna in feugiat.
+                            Mauris turpis quam, auctor ac rutrum eget, feugiat eget metus. Integer sagittis facilisis
+                            nibh ac ornare. Aenean vestibulum orci augue. Suspendisse eleifend massa diam, non
+                            consectetur sem consequat quis. Ut ultricies, magna nec porta bibendum, dolor ligula lacinia
+                            nulla, nec lobortis nibh mauris ac massa. Vivamus vitae auctor magna. Praesent mi purus,
+                            maximus nec feugiat vel, facilisis quis nisl. Duis non finibus justo. Ut posuere leo id
+                            risus pulvinar, pellentesque maximus tellus volutpat. Proin sed turpis condimentum est
+                            maximus lobortis. Nam non tortor enim. Curabitur at ultrices quam, sed pulvinar justo. Donec
+                            et sapien maximus, dignissim mauris elementum, tincidunt risus.
                         </p>
                         <br/>
                         <h3><strong>What do you get by participating?</strong></h3>
-                        <p>Apart from a valuable opportunity to experience the corporate world and polish your skills..
-                            exciting cash prizes will be awarded to winners!</p>
+                        <p>
+                            Mauris porta magna eu leo bibendum, in volutpat dui varius. Maecenas lacinia lacus non
+                            rhoncus hendrerit. Duis varius laoreet est eget egestas. Integer aliquam, ex vehicula
+                            consequat posuere, odio nunc scelerisque libero, at vestibulum neque sem vel erat. Nunc vel
+                            ullamcorper metus, eu placerat lectus. Vivamus fringilla suscipit nunc, id commodo augue
+                            semper at. Aliquam aliquet sit amet ex nec ultrices. Nam nunc lectus, ultrices non
+                            vestibulum id, placerat ut nunc. Phasellus commodo condimentum vestibulum. Proin massa
+                            risus, pulvinar et tortor sodales, malesuada vehicula ligula. Etiam sit amet nisl sed metus
+                            vehicula semper. Nunc condimentum lorem quis varius scelerisque. Pellentesque ac aliquam
+                            nulla, at rhoncus felis. Phasellus auctor ligula et ultrices sagittis. Sed efficitur ligula
+                            et lobortis pulvinar.
+                        </p>
                     </div>
 
                     <div class="col-md-2"></div>
-                </div>
-            </div>
-        </section>
-    </div>
-</div>
-<!--===========winners awards=============-->
-<section class="bg_decrypt section_gap">
-    <div class="container">
-        <div class="d-flex align-items-center mb-5 pb-5">
-            <div class="container">
-                <div class="text-center text-white">
-                    <h2>Prizes</h2>
-                </div>
-            </div>
-        </div>
-        <div class="row text-center">
-            <div class="col-md-4 p-5 bg_prize hvr-grow-rotate">
-                <img src="img/winner.png" alt="First Place"/>
-                <div class="centered">First<br/> Rs 25,000</div>
-            </div>
-            <div class="col-md-4 p-5 bg_prize hvr-grow-rotate">
-                <img src="img/winner.png" alt="Second Place"/>
-                <div class="centered">Second<br/> Rs 15,000</div>
-            </div>
-            <div class="col-md-4 p-5 bg_prize hvr-grow-rotate">
-                <img src="img/winner.png" alt="Third Place"/>
-                <div class="centered">Third<br/> Rs 10,000</div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<!--================Contact info============-->
-<div class="contacts_parallax">
-    <div class="inner">
-        <section class="about_us_area section_gap text-white">
-            <div class="d-flex align-items-center my-2 py-2">
-                <div class="container">
-
-                    <div class="d-flex align-items-center mb-5 pb-5">
-                        <div class="container">
-                            <div class="text-center text-white">
-                                <h2>Contact Us</h2>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row text-center d-flex justify-content-center">
-                        <div class="col-sm-6 col-md-4 hvr-shrink">
-                            <div class="profile-card">
-                                <div class="profile-img">
-                                    <img src="http://s1.1zoom.me/b5346/617/Sherlock_Holmes_Men_Benedict_Cumberbatch_518637_640x960.jpg" alt="Team Image"/>
-                                </div>
-                                <div class="profile-content">
-                                    <h2 class="title">Henry Polles
-                                        <span>078 9966789</span>
-                                    </h2>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 col-md-4 hvr-shrink">
-                            <div class="profile-card">
-                                <div class="profile-img">
-                                    <img src="http://s1.1zoom.me/b5346/617/Sherlock_Holmes_Men_Benedict_Cumberbatch_518637_640x960.jpg" alt="Team Image"/>
-                                </div>
-                                <div class="profile-content">
-                                    <h2 class="title">Henry Polles
-                                        <span>078 9966789</span>
-                                    </h2>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 col-md-4 hvr-shrink">
-                            <div class="profile-card">
-                                <div class="profile-img">
-                                    <img src="http://s1.1zoom.me/b5346/617/Sherlock_Holmes_Men_Benedict_Cumberbatch_518637_640x960.jpg" alt="Team Image"/>
-                                </div>
-                                <div class="profile-content">
-                                    <h2 class="title">Henry Polles
-                                        <span>078 9966789</span>
-                                    </h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </section>
@@ -139,7 +78,7 @@
         <div class="d-flex align-items-center mb-5 pb-5">
             <div class="container">
                 <div class="text-center text-white">
-                    <h2>Ideathon - 2018</h2>
+                    <h2>Decrypt Exhibition - 2018</h2>
                 </div>
             </div>
         </div>
@@ -181,27 +120,6 @@
                 <a href="img/ideathon/img_12.jpg">
                     <img src="img/ideathon/img_12.jpg" class="hvr-shrink" alt="Gallery"/>
                 </a>
-                <a href="img/ideathon/img_13.jpg">
-                    <img src="img/ideathon/img_13.jpg" class="hvr-shrink" alt="Gallery"/>
-                </a>
-                <a href="img/ideathon/img_14.jpg">
-                    <img src="img/ideathon/img_14.jpg" class="hvr-shrink" alt="Gallery"/>
-                </a>
-                <a href="img/ideathon/img_15.jpg">
-                    <img src="img/ideathon/img_15.jpg" class="hvr-shrink" alt="Gallery"/>
-                </a>
-                <a href="img/ideathon/img_16.jpg">
-                    <img src="img/ideathon/img_16.jpg" class="hvr-shrink" alt="Gallery"/>
-                </a>
-                <a href="img/ideathon/img_17.jpg">
-                    <img src="img/ideathon/img_17.jpg" class="hvr-shrink" alt="Gallery"/>
-                </a>
-                <a href="img/ideathon/img_18.jpg">
-                    <img src="img/ideathon/img_18.jpg" class="hvr-shrink" alt="Gallery"/>
-                </a>
-                <a href="img/ideathon/img_19.jpg">
-                    <img src="img/ideathon/img_19.jpg" class="hvr-shrink" alt="Gallery"/>
-                </a>
             </div>
         </div>
     </div>
@@ -212,51 +130,6 @@
 <?php include 'scripts.php' ?>
 
 <script>
-
-    // DO NOT CHANGE SVG INFORMATION
-    let timeline_bg = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='400' height='400' viewBox='0 0 800 800'%3E%3Cg fill='none' " +
-        "stroke='%23383212' stroke-width='1'%3E%3Cpath d='M769 229L1037 260.9M927 880L731 737 520 660 309 538 40 599 295 764 126.5 879.5 40 599-197 " +
-        "493 102 382-31 229 126.5 79.5-69-63'/%3E%3Cpath d='M-31 229L237 261 390 382 603 493 308.5 537.5 101.5 381.5M370 905L295 764'/%3E%3Cpath " +
-        "d='M520 660L578 842 731 737 840 599 603 493 520 660 295 764 309 538 390 382 539 269 769 229 577.5 41.5 370 105 295 -36 126.5 79.5 237 261 " +
-        "102 382 40 599 -69 737 127 880'/%3E%3Cpath d='M520-140L578.5 42.5 731-63M603 493L539 269 237 261 370 105M902 382L539 269M390 382L102 " +
-        "382'/%3E%3Cpath d='M-222 42L126.5 79.5 370 105 539 269 577.5 41.5 927 80 769 229 902 382 603 493 731 737M295-36L577.5 41.5M578 842L295 " +
-        "764M40-201L127 80M102 382L-261 269'/%3E%3C/g%3E%3Cg fill='%23664d00'%3E%3Ccircle cx='769' cy='229' r='5'/%3E%3Ccircle cx='539' cy='269' " +
-        "r='5'/%3E%3Ccircle cx='603' cy='493' r='5'/%3E%3Ccircle cx='731' cy='737' r='5'/%3E%3Ccircle cx='520' cy='660' r='5'/%3E%3Ccircle cx='309' " +
-        "cy='538' r='5'/%3E%3Ccircle cx='295' cy='764' r='5'/%3E%3Ccircle cx='40' cy='599' r='5'/%3E%3Ccircle cx='102' cy='382' r='5'/%3E%3Ccircle " +
-        "cx='127' cy='80' r='5'/%3E%3Ccircle cx='370' cy='105' r='5'/%3E%3Ccircle cx='578' cy='42' r='5'/%3E%3Ccircle cx='237' cy='261' r='5'/%3E%3Ccircle " +
-        "cx='390' cy='382' r='5'/%3E%3C/g%3E%3C/svg%3E";
-
-    // DO NOT CHANGE SVG INFORMATION
-    let contact_bg = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='2000' height='2000' viewBox='0 0 800 800'%3E%3Cg fill='none' %3E%3Cg " +
-        "stroke='%23026' stroke-width='17'%3E%3Cline x1='-8' y1='-8' x2='808' y2='808'/%3E%3Cline x1='-8' y1='792' x2='808' y2='1608'/%3E%3Cline x1='-8' " +
-        "y1='-808' x2='808' y2='8'/%3E%3C/g%3E%3Cg stroke='%23002163' stroke-width='16'%3E%3Cline x1='-8' y1='767' x2='808' y2='1583'/%3E%3Cline x1='-8' " +
-        "y1='17' x2='808' y2='833'/%3E%3Cline x1='-8' y1='-33' x2='808' y2='783'/%3E%3Cline x1='-8' y1='-783' x2='808' y2='33'/%3E%3C/g%3E%3Cg stroke='%23002060' " +
-        "stroke-width='15'%3E%3Cline x1='-8' y1='742' x2='808' y2='1558'/%3E%3Cline x1='-8' y1='42' x2='808' y2='858'/%3E%3Cline x1='-8' y1='-58' x2='808' " +
-        "y2='758'/%3E%3Cline x1='-8' y1='-758' x2='808' y2='58'/%3E%3C/g%3E%3Cg stroke='%23001f5c' stroke-width='14'%3E%3Cline x1='-8' y1='67' x2='808' " +
-        "y2='883'/%3E%3Cline x1='-8' y1='717' x2='808' y2='1533'/%3E%3Cline x1='-8' y1='-733' x2='808' y2='83'/%3E%3Cline x1='-8' y1='-83' x2='808' " +
-        "y2='733'/%3E%3C/g%3E%3Cg stroke='%23001e59' stroke-width='13'%3E%3Cline x1='-8' y1='92' x2='808' y2='908'/%3E%3Cline x1='-8' y1='692' x2='808' " +
-        "y2='1508'/%3E%3Cline x1='-8' y1='-108' x2='808' y2='708'/%3E%3Cline x1='-8' y1='-708' x2='808' y2='108'/%3E%3C/g%3E%3Cg stroke='%23001d56' " +
-        "stroke-width='12'%3E%3Cline x1='-8' y1='667' x2='808' y2='1483'/%3E%3Cline x1='-8' y1='117' x2='808' y2='933'/%3E%3Cline x1='-8' y1='-133' x2='808' " +
-        "y2='683'/%3E%3Cline x1='-8' y1='-683' x2='808' y2='133'/%3E%3C/g%3E%3Cg stroke='%23001c53' stroke-width='11'%3E%3Cline x1='-8' y1='642' x2='808' " +
-        "y2='1458'/%3E%3Cline x1='-8' y1='142' x2='808' y2='958'/%3E%3Cline x1='-8' y1='-158' x2='808' y2='658'/%3E%3Cline x1='-8' y1='-658' x2='808' " +
-        "y2='158'/%3E%3C/g%3E%3Cg stroke='%23001b4f' stroke-width='10'%3E%3Cline x1='-8' y1='167' x2='808' y2='983'/%3E%3Cline x1='-8' y1='617' x2='808'" +
-        " y2='1433'/%3E%3Cline x1='-8' y1='-633' x2='808' y2='183'/%3E%3Cline x1='-8' y1='-183' x2='808' y2='633'/%3E%3C/g%3E%3Cg stroke='%23001a4c' " +
-        "stroke-width='9'%3E%3Cline x1='-8' y1='592' x2='808' y2='1408'/%3E%3Cline x1='-8' y1='192' x2='808' y2='1008'/%3E%3Cline x1='-8' y1='-608' x2='808' " +
-        "y2='208'/%3E%3Cline x1='-8' y1='-208' x2='808' y2='608'/%3E%3C/g%3E%3Cg stroke='%23001949' stroke-width='8'%3E%3Cline x1='-8' y1='567' x2='808' " +
-        "y2='1383'/%3E%3Cline x1='-8' y1='217' x2='808' y2='1033'/%3E%3Cline x1='-8' y1='-233' x2='808' y2='583'/%3E%3Cline x1='-8' y1='-583' x2='808' " +
-        "y2='233'/%3E%3C/g%3E%3Cg stroke='%23001846' stroke-width='7'%3E%3Cline x1='-8' y1='242' x2='808' y2='1058'/%3E%3Cline x1='-8' y1='542' x2='808' " +
-        "y2='1358'/%3E%3Cline x1='-8' y1='-558' x2='808' y2='258'/%3E%3Cline x1='-8' y1='-258' x2='808' y2='558'/%3E%3C/g%3E%3Cg stroke='%23001743' " +
-        "stroke-width='6'%3E%3Cline x1='-8' y1='267' x2='808' y2='1083'/%3E%3Cline x1='-8' y1='517' x2='808' y2='1333'/%3E%3Cline x1='-8' y1='-533' " +
-        "x2='808' y2='283'/%3E%3Cline x1='-8' y1='-283' x2='808' y2='533'/%3E%3C/g%3E%3Cg stroke='%2300163f' stroke-width='5'%3E%3Cline x1='-8' " +
-        "y1='292' x2='808' y2='1108'/%3E%3Cline x1='-8' y1='492' x2='808' y2='1308'/%3E%3Cline x1='-8' y1='-308' x2='808' y2='508'/%3E%3Cline x1='-8'" +
-        " y1='-508' x2='808' y2='308'/%3E%3C/g%3E%3Cg stroke='%2300153c' stroke-width='4'%3E%3Cline x1='-8' y1='467' x2='808' y2='1283'/%3E%3Cline x1='-8' " +
-        "y1='317' x2='808' y2='1133'/%3E%3Cline x1='-8' y1='-333' x2='808' y2='483'/%3E%3Cline x1='-8' y1='-483' x2='808' y2='333'/%3E%3C/g%3E%3Cg " +
-        "stroke='%23001439' stroke-width='3'%3E%3Cline x1='-8' y1='342' x2='808' y2='1158'/%3E%3Cline x1='-8' y1='442' x2='808' y2='1258'/%3E%3Cline " +
-        "x1='-8' y1='-458' x2='808' y2='358'/%3E%3Cline x1='-8' y1='-358' x2='808' y2='458'/%3E%3C/g%3E%3Cg stroke='%23001336' stroke-width='2'%3E%3Cline " +
-        "x1='-8' y1='367' x2='808' y2='1183'/%3E%3Cline x1='-8' y1='417' x2='808' y2='1233'/%3E%3Cline x1='-8' y1='-433' x2='808' y2='383'/%3E%3Cline " +
-        "x1='-8' y1='-383' x2='808' y2='433'/%3E%3C/g%3E%3Cg stroke='%23013' stroke-width='1'%3E%3Cline x1='-8' y1='392' x2='808' y2='1208'/%3E%3Cline " +
-        "x1='-8' y1='-408' x2='808' y2='408'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E";
-
 
     // Gallery Activation
     gallery = document.getElementById('lightgallery');
@@ -275,16 +148,8 @@
 
     // Parallax
     $(document).ready(function () {
-        $('.timeline_parallax').parallax({
-            imageSrc:timeline_bg
-        });
-
         $('.ideathon_parallax').parallax({
-            imageSrc: 'img/events/ideathon_header.jpg'
-        });
-
-        $('.contacts_parallax').parallax({
-            imageSrc: contact_bg
+            imageSrc: 'img/exhibition/exhibitionbackground.jpg'
         });
     });
 </script>
